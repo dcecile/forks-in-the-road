5ef998ee110196541e59ce95d76538145ef33360
Dan Cecile <dancecile@gmail.com>
2017-12-05 18:38:19 -0500

    Set Ruby version to 2.4.2

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Default mailer
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Base class for all records
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
