c13d23fd59a5ef868110190a74ede0b458ddc33a
Dan Cecile <dancecile@gmail.com>
2017-12-26 13:08:32 -0500

    Flatten JavaScript directories

--- app/packs/application.js
+++ app/packs/application.js
@@ -1,7 +1,8 @@
+import "babel-polyfill"
 import ReactRailsUJS from "react_ujs"
 import "Style.sass"
 
 console.log("Webpacker initialized")
 
 // Support component names relative to this directory:
-ReactRailsUJS.useContext(require.context("components", true))
+ReactRailsUJS.useContext(require.context("../components", true))
--- app/packs/server_rendering.js
+++ app/packs/server_rendering.js
@@ -1,8 +1,7 @@
-import "babel-polyfill"
 import ReactRailsUJS from "react_ujs"
 import "Style.sass"
 
 console.log("Webpacker initialized")
 
 // Support component names relative to this directory:
-ReactRailsUJS.useContext(require.context("components", true))
+ReactRailsUJS.useContext(require.context("../components", true))
