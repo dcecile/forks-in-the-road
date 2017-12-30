04e36b63e6585a4bb0d8a4d0793cb2c48137628d
Dan Cecile <dancecile@gmail.com>
2017-12-30 12:04:58 -0500

    Skip building the SSR pack for development 🔨
    For #11

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,10 +2,14 @@ const environment = require("./environment")
 
 const config = environment.toWebpackConfig()
 
+// Show build stats
 // https://webpack.js.org/configuration/stats/
 config.devServer.stats = {
   errorDetails: true,
   timings: true
 }
+
+// Skip SSR for development
+delete config.entry.server_rendering
 
 module.exports = config
