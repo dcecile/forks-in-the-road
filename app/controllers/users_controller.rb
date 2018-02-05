1051676e98bef79c7c71701b6d4f920319f96da5
Dan Cecile <dancecile@gmail.com>
2018-02-05 10:21:15 +0800

    Associate a GitHub authorization with a DB user 🏗️
    For #1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
   def authorize_callback
     token = application_github_client.exchange_code_for_token(params[:code])
     user_github_client = create_user_github_client(token)
-    render(json: user_github_client.user)
+    user = find_or_create_user(user_github_client.user)
+    render(json: user)
   end
 
   def create_user_github_client(token)
@@ -25,6 +26,35 @@ class UsersController < ApplicationController
       client_secret: application_github_client.client_secret,
       access_token: token.access_token
     )
+  end
+
+  def find_or_create_user(github_user)
+    github_id = github_user.id
+    info = get_github_user_info(github_user)
+    found_user = User.find_by(github_id: github_id)
+    if found_user
+      update_user(found_user, info)
+    else
+      create_user(github_id, info)
+    end
+  end
+
+  def get_github_user_info(github_user)
+    {
+      github_login: github_user.login,
+      github_avatar_url: github_user.avatar_url
+    }
+  end
+
+  def update_user(found_user, info)
+    logger.info "Found user (#{found_user.to_json}) and updating (#{info})"
+    found_user.update!(info)
+    found_user
+  end
+
+  def create_user(github_id, info)
+    logger.info "Creating new user for #{github_id} (#{info})"
+    User.create!(github_id: github_id, **info)
   end
 
   private
