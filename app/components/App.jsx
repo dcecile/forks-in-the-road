168499c585ba79e83e71349860f1a067c9360266
Dan Cecile <dancecile@gmail.com>
2018-03-13 11:14:47 +0800

    Split out app component state ♻️
    For #13

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,210 +1,124 @@
-import PropTypes from "prop-types"
 import React from "react"
-import queryString from "query-string"
 import { Redirect, Switch } from "react-router-dom"
 import { Route } from "react-router"
 
 import ComparisonIndex from "ComparisonIndex"
 import Comparison from "Comparison"
 import RouteNotFound from "RouteNotFound"
-import Server from "Server"
 import SignIn from "SignIn"
-import Timing from "Timing"
 import { HeaderSlot } from "Header"
+import AppState from "AppState"
 
-const UserStorageKey = "user"
+export default AppState.renderWith(render)
 
-export default class App extends React.Component {
-  static childContextTypes = {
-    headerSlot: PropTypes.instanceOf(HTMLElement)
-  }
+function render({
+  user,
+  server,
+  isUserSigningInChanging,
+  isUserSigningIn,
+  isUserSigningOut,
+  onHeaderSlotRef,
+  onUserSignIn,
+  onUserSignOut
+}) {
+  const isUserSigningOutClassName = isUserSigningOut
+    ? "App_main__isUserSigningOut"
+    : ""
+  const appClassName = `App_main ${isUserSigningOutClassName}`
 
-  constructor() {
-    super()
-    const user = this.readUserFromLocalStorage()
-    this.state = {
-      headerSlot: null,
-      user,
-      isUserSigningInChanging: false,
-      isUserSigningIn: false,
-      isUserSigningOut: false
-    }
-    this.server = new Server(user)
-  }
+  const renderSignInRequiredPartial = renderSignInRequired(
+    user,
+    isUserSigningInChanging,
+    isUserSigningIn,
+    onUserSignIn
+  )
 
-  get history() {
-    return this.props.history
-  }
+  const renderRoutePartial = renderRoute(
+    renderSignInRequiredPartial,
+    appClassName,
+    server
+  )
 
-  get user() {
-    return this.state.user
-  }
-
-  get isUserSigningInChanging() {
-    return this.state.isUserSigningInChanging
-  }
-
-  get isUserSigningIn() {
-    return this.state.isUserSigningIn
-  }
-
-  get isUserSigningOut() {
-    return this.state.isUserSigningOut
-  }
-
-  get isUserSigningOutClassName() {
-    return this.isUserSigningOut ? "App_main__isUserSigningOut" : ""
-  }
-
-  getChildContext() {
-    return { headerSlot: this.state.headerSlot }
-  }
-
-  readUserFromLocalStorage() {
-    return JSON.parse(window.localStorage.getItem(UserStorageKey) || null)
-  }
-
-  writeUserToLocalStorage(user) {
-    window.localStorage.setItem(UserStorageKey, JSON.stringify(user))
-  }
-
-  componentDidMount() {
-    this.setState({
-      headerSlot: this.headerSlot
-    })
-    this.handleUserSignInCallback()
-    this.handleOtherTabUserChange()
-  }
-
-  async handleUserSignIn() {
-    this.setState({ isUserSigningInChanging: true })
-    await Timing.appUserSigningInChanging()
-    this.setState({ isUserSigningInChanging: false, isUserSigningIn: true })
-    const clientID = process.env.GITHUB_CLIENT_ID
-    const params = queryString.stringify({
-      client_id: clientID,
-      redirect_uri: window.location.href
-    })
-    const authorizeURL = `https://github.com/login/oauth/authorize?${params}`
-    window.location.href = authorizeURL
-  }
-
-  async handleUserSignInCallback() {
-    const code = this.parseSearchParameter()
-    if (!code) {
-      return
-    }
-
-    this.setState({ isUserSigningIn: true })
-    console.log("Authorizing", code)
-    const response = await this.server.post("/users/authorize", { code })
-    const user = response.data
-    console.log("Authorized", user)
-    this.setState({ isUserSigningInChanging: true })
-    await Timing.appUserSigningInChanging()
-    this.setState({ isUserSigningInChanging: false })
-    this.handleUserChange(user)
-    this.removeSearchParameter()
-    this.setState({ isUserSigningIn: false })
-  }
-
-  parseSearchParameter() {
-    const parameters = queryString.parse(window.location.search)
-    return parameters["code"]
-  }
-
-  removeSearchParameter() {
-    this.history.replace(window.location.pathname)
-  }
-
-  handleUserChange(user) {
-    this.writeUserToLocalStorage(user)
-    this.server.user = user
-    this.setState({ user })
-  }
-
-  async handleUserSignOut() {
-    this.setState({ isUserSigningOut: true })
-    await Timing.appUserSigningOut()
-    this.handleUserChange(null)
-    this.setState({ isUserSigningOut: false })
-  }
-
-  handleOtherTabUserChange() {
-    window.addEventListener("storage", event => {
-      if (event.key === UserStorageKey) {
-        const user = JSON.parse(event.newValue)
-        console.log("Other tab user change", user)
-        this.server.user = user
-        this.setState({ user })
-      }
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <HeaderSlot
-          user={this.user}
-          isUserSigningInChanging={this.isUserSigningInChanging}
-          isUserSigningIn={this.isUserSigningIn}
-          onRef={headerSlot => (this.headerSlot = headerSlot)}
-          onUserSignIn={() => this.handleUserSignIn()}
-          onUserSignOut={() => this.handleUserSignOut()}
+  return (
+    <div className="App">
+      {renderHeader(
+        user,
+        isUserSigningInChanging,
+        isUserSigningIn,
+        onHeaderSlotRef,
+        onUserSignIn,
+        onUserSignOut
+      )}
+      <Switch>
+        <Redirect exact from="/" to="/app/dashboard" />
+        <Route
+          exact
+          path="/app/dashboard"
+          render={renderRoutePartial(ComparisonIndex)}
         />
-        <Switch>
-          <Redirect exact from="/" to="/app/dashboard" />
-          <Route
-            exact
-            path="/app/dashboard"
-            render={routeProps => this.renderComparisonIndex(routeProps)}
-          />
-          <Route
-            path="/app/comparison/:id"
-            render={routeProps => this.renderComparison(routeProps)}
-          />
-          <Route component={RouteNotFound} />
-        </Switch>
-      </div>
-    )
-  }
+        <Route
+          path="/app/comparison/:id"
+          render={renderRoutePartial(Comparison)}
+        />
+        <Route component={RouteNotFound} />
+      </Switch>
+    </div>
+  )
+}
 
-  renderComparisonIndex(routeProps) {
-    return this.renderSignInRequired(
-      <ComparisonIndex
-        className={`App_main ${this.isUserSigningOutClassName}`}
-        server={this.server}
-        {...routeProps}
-      />
-    )
-  }
+function renderHeader(
+  user,
+  isUserSigningInChanging,
+  isUserSigningIn,
+  onHeaderSlotRef,
+  onUserSignIn,
+  onUserSignOut
+) {
+  return (
+    <HeaderSlot
+      user={user}
+      isUserSigningInChanging={isUserSigningInChanging}
+      isUserSigningIn={isUserSigningIn}
+      onRef={onHeaderSlotRef}
+      onUserSignIn={onUserSignIn}
+      onUserSignOut={onUserSignOut}
+    />
+  )
+}
 
-  renderComparison(routeProps) {
-    return this.renderSignInRequired(
-      <Comparison
-        className={`App_main ${this.isUserSigningOutClassName}`}
-        server={this.server}
-        {...routeProps}
-      />
+function renderRoute(renderSignInRequiredPartial, appClassName, server) {
+  return Component => routeProps =>
+    renderSignInRequiredPartial(
+      <Component className={appClassName} server={server} {...routeProps} />
     )
-  }
+}
 
-  renderSignInRequired(component) {
-    if (this.isUserSigningIn || !this.user) {
-      return this.renderSignIn()
+function renderSignInRequired(
+  user,
+  isUserSigningInChanging,
+  isUserSigningIn,
+  onUserSignIn
+) {
+  return component => {
+    if (isUserSigningIn || !user) {
+      return renderSignIn(
+        isUserSigningInChanging,
+        isUserSigningIn,
+        onUserSignIn
+      )
     } else {
       return component
     }
   }
+}
 
-  renderSignIn() {
-    return (
-      <SignIn
-        className="App_main"
-        isUserSigningInChanging={this.isUserSigningInChanging}
-        isUserSigningIn={this.isUserSigningIn}
-        onUserSignIn={() => this.handleUserSignIn()}
-      />
-    )
-  }
+function renderSignIn(isUserSigningInChanging, isUserSigningIn, onUserSignIn) {
+  return (
+    <SignIn
+      className="App_main"
+      isUserSigningInChanging={isUserSigningInChanging}
+      isUserSigningIn={isUserSigningIn}
+      onUserSignIn={onUserSignIn}
+    />
+  )
 }
---
+++ app/components/AppState.js
@@ -0,0 +1,149 @@
+import StateComponent from "StateComponent"
+import PropTypes from "prop-types"
+import queryString from "query-string"
+
+import Server from "Server"
+import Timing from "Timing"
+
+const UserStorageKey = "user"
+
+export default class AppState extends StateComponent {
+  static childContextTypes = {
+    headerSlot: PropTypes.instanceOf(HTMLElement)
+  }
+
+  constructor(props) {
+    super(props)
+    const user = this.readUserFromLocalStorage()
+    this.state = {
+      headerSlot: null,
+      user,
+      isUserSigningInChanging: false,
+      isUserSigningIn: false,
+      isUserSigningOut: false
+    }
+    this.server = new Server(user)
+    this.headerSlot = null
+  }
+
+  get history() {
+    return this.props.history
+  }
+
+  get user() {
+    return this.state.user
+  }
+
+  get isUserSigningInChanging() {
+    return this.state.isUserSigningInChanging
+  }
+
+  get isUserSigningIn() {
+    return this.state.isUserSigningIn
+  }
+
+  get isUserSigningOut() {
+    return this.state.isUserSigningOut
+  }
+
+  getChildContext() {
+    return { headerSlot: this.state.headerSlot }
+  }
+
+  readUserFromLocalStorage() {
+    return JSON.parse(window.localStorage.getItem(UserStorageKey) || null)
+  }
+
+  writeUserToLocalStorage(user) {
+    window.localStorage.setItem(UserStorageKey, JSON.stringify(user))
+  }
+
+  componentDidMount() {
+    this.setState({
+      headerSlot: this.headerSlot
+    })
+    this.handleUserSignInCallback()
+    this.handleOtherTabUserChange()
+  }
+
+  async handleUserSignIn() {
+    this.setState({ isUserSigningInChanging: true })
+    await Timing.appUserSigningInChanging()
+    this.setState({ isUserSigningInChanging: false, isUserSigningIn: true })
+    const clientID = process.env.GITHUB_CLIENT_ID
+    const params = queryString.stringify({
+      client_id: clientID,
+      redirect_uri: window.location.href
+    })
+    const authorizeURL = `https://github.com/login/oauth/authorize?${params}`
+    window.location.href = authorizeURL
+  }
+
+  async handleUserSignInCallback() {
+    const code = this.parseSearchParameter()
+    if (!code) {
+      return
+    }
+
+    this.setState({ isUserSigningIn: true })
+    console.log("Authorizing", code)
+    const response = await this.server.post("/users/authorize", { code })
+    const user = response.data
+    console.log("Authorized", user)
+    this.setState({ isUserSigningInChanging: true })
+    await Timing.appUserSigningInChanging()
+    this.setState({ isUserSigningInChanging: false })
+    this.handleUserChange(user)
+    this.removeSearchParameter()
+    this.setState({ isUserSigningIn: false })
+  }
+
+  parseSearchParameter() {
+    const parameters = queryString.parse(window.location.search)
+    return parameters["code"]
+  }
+
+  removeSearchParameter() {
+    this.history.replace(window.location.pathname)
+  }
+
+  handleUserChange(user) {
+    this.writeUserToLocalStorage(user)
+    this.server.user = user
+    this.setState({ user })
+  }
+
+  async handleUserSignOut() {
+    this.setState({ isUserSigningOut: true })
+    await Timing.appUserSigningOut()
+    this.handleUserChange(null)
+    this.setState({ isUserSigningOut: false })
+  }
+
+  handleOtherTabUserChange() {
+    window.addEventListener("storage", event => {
+      if (event.key === UserStorageKey) {
+        const user = JSON.parse(event.newValue)
+        console.log("Other tab user change", user)
+        this.server.user = user
+        this.setState({ user })
+      }
+    })
+  }
+
+  renderState() {
+    return {
+      headerSlot: this.headerSlot,
+      user: this.user,
+      server: this.server,
+      isUserSigningInChanging: this.isUserSigningInChanging,
+      isUserSigningIn: this.isUserSigningIn,
+      isUserSigningOut: this.isUserSigningOut,
+      onHeaderSlotRef: headerSlot => (this.headerSlot = headerSlot),
+      onUserSignIn: user => this.handleUserSignIn(user),
+      onUserSignOut: () => this.handleUserSignOut()
+    }
+  }
+}
+
+AppState.renderWith = StateComponent.renderWithComponent(AppState)
